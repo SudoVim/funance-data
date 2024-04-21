334224f8e5d3420238e3bddcfb38d0ab04d16cf6
Michael Renken <michaelarenken@gmail.com>
2024-04-21 09:05:12 -0500

    Add elasticsearch and docker-compose

---
+++ funance_data/client.py
@@ -0,0 +1,21 @@
+import os
+import threading
+import elasticsearch
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app_local = threading.local()
+
+
+def elastic_client():
+    """
+    connect and return the elasticsearch client object
+    """
+    if not hasattr(app_local, "global_elastic_client"):
+        app_local.global_elastic_client = elasticsearch.Elasticsearch(
+            [os.getenv("ELASTICSEARCH_URL") or "http://localhost:9200"],
+            http_auth=(os.getenv("ELASTICSEARCH_USERNAME"), os.getenv("ELASTICSEARCH_PASSWORD")),
+        )
+
+    return app_local.global_elastic_client

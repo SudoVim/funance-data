7855e39918b49af866973e8511cbc21388b36ece
Michael Renken <michaelarenken@gmail.com>
2024-04-22 08:14:18 -0500

    Add type checking with mypy

--- funance_data/config.py
+++ funance_data/config.py
@@ -5,12 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 CONFIG_VALUES = {
-    "elasticsearch": {
-        "url": os.getenv("ELASTICSEARCH_URL") or "http://localhost:9200",
-        "username": os.getenv("ELASTICSEARCH_USERNAME") or "elastic",
-        "password": os.getenv("ELASTICSEARCH_PASSWORD") or "",
-        "index_prefix": os.getenv("ELASTICSEARCH_INDEX_PREFIX") or "",
-    },
+    "elasticsearch.url": os.getenv("ELASTICSEARCH_URL") or "http://localhost:9200",
+    "elasticsearch.username": os.getenv("ELASTICSEARCH_USERNAME") or "elastic",
+    "elasticsearch.password": os.getenv("ELASTICSEARCH_PASSWORD") or "",
+    "elasticsearch.index_prefix": os.getenv("ELASTICSEARCH_INDEX_PREFIX") or "",
 }
 
 
@@ -18,12 +16,4 @@ def config(key: str) -> Optional[str]:
     """
     Get configuration value denoted by the given ``key``.
     """
-    parts = key.split(".")
-    c = CONFIG_VALUES
-    for part in parts:
-        if part not in c:
-            return None
-
-        c = c[part]
-
-    return c
+    return CONFIG_VALUES[key]

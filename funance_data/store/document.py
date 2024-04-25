ca5248baac8a5d1aadac6d6d947aa9bbf07a08ea
Michael Renken <michaelarenken@gmail.com>
2024-04-25 10:08:42 -0500

    Add store module

---
+++ funance_data/store/document.py
@@ -0,0 +1,41 @@
+from typing import Any, Mapping, TypeVar
+from typing_extensions import Self
+
+
+class Document:
+    """
+    Base class for all objects that are meant to be used with
+    :class:`DataStore`.
+
+    .. autoattribute:: data
+    .. autoattribute:: source
+
+    .. automethod:: encode
+    .. automethod:: decode
+    """
+
+    #: The raw underlying data
+    data: Mapping[str, Any]
+
+    #: The document source
+    source: Mapping[str, Any]
+
+    def __init__(self, data: Mapping[str, Any]) -> None:
+        self.data = data
+        self.source = self.data.get("_source", {})
+
+    def encode(self) -> Mapping[str, Any]:
+        """
+        Encode the data in this document to be stored in elasticsearch.
+        """
+        return self.data
+
+    @classmethod
+    def decode(cls, data: Mapping[str, Any]) -> Self:
+        """
+        Decode the given ``data`` into this document.
+        """
+        return cls(data)
+
+
+D = TypeVar("D", bound=Document)

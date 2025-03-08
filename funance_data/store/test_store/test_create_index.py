1916e19212efceaf009a7f419d101623751e9965
Michael Renken <michaelarenken@gmail.com>
2025-03-08 10:22:15 -0600

    Ran format

--- funance_data/store/test_store/test_create_index.py
+++ funance_data/store/test_store/test_create_index.py
@@ -5,14 +5,20 @@ class TestCreateIndex(StoreTestCase):
     def test_does_not_exist(self) -> None:
         self.mock_client.indices.exists.return_value = False
         self.store_class.create_index(client=self.mock_client)
-        self.mock_client.indices.exists.assert_called_once_with(index=self.store.get_index_name())
-        self.mock_client.indices.create.assert_called_once_with(index=self.store.get_index_name())
+        self.mock_client.indices.exists.assert_called_once_with(
+            index=self.store.get_index_name()
+        )
+        self.mock_client.indices.create.assert_called_once_with(
+            index=self.store.get_index_name()
+        )
         self.mock_client.indices.put_mapping.assert_not_called()
 
     def test_exists(self) -> None:
         self.mock_client.indices.exists.return_value = True
         self.store_class.create_index(client=self.mock_client)
-        self.mock_client.indices.exists.assert_called_once_with(index=self.store.get_index_name())
+        self.mock_client.indices.exists.assert_called_once_with(
+            index=self.store.get_index_name()
+        )
         self.mock_client.indices.create.assert_not_called()
         self.mock_client.indices.put_mapping.assert_not_called()
 
@@ -20,7 +26,9 @@ class TestCreateIndex(StoreTestCase):
         self.store_class.INDEX_SPEC = {"my": "spec"}
         self.mock_client.indices.exists.return_value = True
         self.store_class.create_index(client=self.mock_client)
-        self.mock_client.indices.exists.assert_called_once_with(index=self.store.get_index_name())
+        self.mock_client.indices.exists.assert_called_once_with(
+            index=self.store.get_index_name()
+        )
         self.mock_client.indices.create.assert_not_called()
         self.mock_client.indices.put_mapping.assert_called_once_with(
             index=self.store.get_index_name(), body={"my": "spec"}
